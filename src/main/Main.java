17c19cac1c9dd340eab60dffba3178986ca4c1ef
D4rk-h <125494273+D4rk-h@users.noreply.github.com>
2024-12-16 21:35:39 +0000

    Add Main class

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.ui.ConsoleAppUI;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleAppUI app = new ConsoleAppUI();
+        app.start();
+    }
+}
